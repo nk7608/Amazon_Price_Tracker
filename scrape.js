42960cb4906597a6044f3bdf886c76c4bab7333d
Nayana Singh <nayanak872@gmail.com>
2022-08-11 19:35:23 +0530

    done

--- scrape.js
+++ scrape.js
@@ -1,7 +1,9 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
+
 require("dotenv").config();
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
